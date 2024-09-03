8942b54e3dad26444c7fca88ff8d4eec182f7c16
joony.kim <bestheroz@gmail.com>
2024-09-03 13:15:48 +0900

    🔧duration: 2_000,

--- src/main.ts
+++ src/main.ts
@@ -16,11 +16,14 @@ app.use(router).use(pinia).use(vuetify);
 const notivue = createNotivue({
   position: "top-right",
   notifications: {
-    global: {
-      duration: 5_000,
-    },
     success: {
       duration: 2_000,
+    },
+    info: {
+      duration: 4_000,
+    },
+    warning: {
+      duration: 5_000,
     },
     error: {
       duration: 10_000,
