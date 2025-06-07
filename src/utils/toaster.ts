4226a9df5c2d40cbbd045bca13b02bfd046de7e0
joony.kim <bestheroz@gmail.com>
2025-06-07 13:40:47 +0900

    ♻️

--- src/utils/toaster.ts
+++ src/utils/toaster.ts
@@ -2,13 +2,13 @@ import { toast, ToasterProps } from "vue-sonner";
 import "vue-sonner/style.css";
 
 export const toastSuccess = (message: string, options?: ToasterProps) =>
-  toast.success(message, { ...options, duration: 2_000 });
+  toast.success(message, { ...options, duration: 2_000, closeButton: true });
 
 export const toastInfo = (message: string, options?: ToasterProps) =>
-  toast.info(message, { ...options, duration: 4_000 });
+  toast.info(message, { ...options, duration: 4_000, closeButton: true });
 
 export const toastWarning = (message: string, options?: ToasterProps) =>
-  toast.warning(message, { ...options, duration: 6_000 });
+  toast.warning(message, { ...options, duration: 6_000, closeButton: true });
 
 export const toastError = (message: string, options?: ToasterProps) =>
-  toast.error(message, { ...options, duration: 10_000 });
+  toast.error(message, { ...options, duration: 10_000, closeButton: true });
